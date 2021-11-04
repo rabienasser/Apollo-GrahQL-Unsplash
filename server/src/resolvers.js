47e22945a52450ec313aefa8a81d7bcae6a46c73
Rabie <robienasser13@gmail.com>
2021-11-03 21:34:40 -0400

    created schema for user endpoints

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,3 +1,24 @@
-const resolvers = {};
+const resolvers = {
+   Query: {
+      userProfile: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashAPI.getUserProfile(username);
+      },
+      userPortfolioLink: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashAPI.getUserPortfolioLink(username);
+      },
+      userPhotos: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashAPI.getUserPhotos(username);
+      },
+      userLikedPhotos: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashAPI.getUserLikedPhotos(username);
+      },
+      userCollections: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashAPI.getUserCollections(username);
+      },
+      userStatistics: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashAPI.getUserStatistics(username);
+      },
+   },
+};
 
 module.exports = resolvers;
