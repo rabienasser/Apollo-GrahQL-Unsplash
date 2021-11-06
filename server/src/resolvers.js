6de065a974e7134d51ccf00e678ba8f5d15d689f
Rabie <robienasser13@gmail.com>
2021-11-06 14:58:35 -0400

    created schema for searching users/collections/photos

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,49 +1,80 @@
 const resolvers = {
-  Query: {
-    userProfile: (_, { username }, { dataSources }) => {
-      return dataSources.unsplashAPI.getUserProfile(username);
-    },
-    userPortfolioLink: (_, { username }, { dataSources }) => {
-      return dataSources.unsplashAPI.getUserPortfolioLink(username);
-    },
-    userPhotos: (_, { username }, { dataSources }) => {
-      return dataSources.unsplashAPI.getUserPhotos(username);
-    },
-    userLikedPhotos: (_, { username }, { dataSources }) => {
-      return dataSources.unsplashAPI.getUserLikedPhotos(username);
-    },
-    userCollections: (_, { username }, { dataSources }) => {
-      return dataSources.unsplashAPI.getUserCollections(username);
-    },
-    userStatistics: (_, { username }, { dataSources }) => {
-      return dataSources.unsplashAPI.getUserStatistics(username);
-    },
+   Query: {
+      //  USERS
+      userProfile: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashUsersAPI.getUserProfile(username);
+      },
+      userPortfolioLink: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashUsersAPI.getUserPortfolioLink(username);
+      },
+      userPhotos: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashUsersAPI.getUserPhotos(username);
+      },
+      userLikedPhotos: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashUsersAPI.getUserLikedPhotos(username);
+      },
+      userCollections: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashUsersAPI.getUserCollections(username);
+      },
+      userStatistics: (_, { username }, { dataSources }) => {
+         return dataSources.unsplashUsersAPI.getUserStatistics(username);
+      },
 
-    getAllPhotos: (_, { currentPage, limit, order }, { dataSources }) => {
-      return dataSources.unsplashPhotosAPI.getAllPhotos(
-        currentPage,
-        limit,
-        order
-      );
-    },
-    getPhoto: (_, { id }, { dataSources }) => {
-      return dataSources.unsplashPhotosAPI.getPhoto(id);
-    },
-    getRandomPhoto: (_, { photoCount, query, username }, { dataSources }) => {
-      return dataSources.unsplashPhotosAPI.getRandomPhoto(
-        photoCount,
-        query,
-        username
-      );
-    },
-    getPhotoStatistics: (_, { id, resolution, quantity }, { dataSources }) => {
-      return dataSources.unsplashPhotosAPI.getPhotoStatistics(
-        id,
-        resolution,
-        quantity
-      );
-    },
-  },
+      // PHOTOS
+      getAllPhotos: (_, { currentPage, limit, order }, { dataSources }) => {
+         return dataSources.unsplashPhotosAPI.getAllPhotos(
+            currentPage,
+            limit,
+            order
+         );
+      },
+      getPhoto: (_, { id }, { dataSources }) => {
+         return dataSources.unsplashPhotosAPI.getPhoto(id);
+      },
+      getRandomPhoto: (_, { photoCount, query, username }, { dataSources }) => {
+         return dataSources.unsplashPhotosAPI.getRandomPhoto(
+            photoCount,
+            query,
+            username
+         );
+      },
+      getPhotoStatistics: (
+         _,
+         { id, resolution, quantity },
+         { dataSources }
+      ) => {
+         return dataSources.unsplashPhotosAPI.getPhotoStatistics(
+            id,
+            resolution,
+            quantity
+         );
+      },
+
+      // SEARCH
+      searchPhotos: (_, { query, page, per_page }, { dataSources }) => {
+         return dataSources.unsplashSearchAPI.searchPhotos(
+            query,
+            page,
+            per_page
+         );
+      },
+
+      searchCollections: (_, { query, page, per_page }, { dataSources }) => {
+         return dataSources.unsplashSearchAPI.searchCollections(
+            query,
+            page,
+            per_page
+         );
+      },
+
+      searchUsers: (_, { query, page, per_page }, { dataSources }) => {
+         return dataSources.unsplashSearchAPI.searchUsers(
+            query,
+            page,
+            per_page
+         );
+      },
+   },
 };
 
 module.exports = resolvers;
